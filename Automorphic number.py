766b15face58b7aa8ff09a777354f581c54c9910
swathi156 <swathiprabhu016@gmail.com>
2025-04-12 11:37:57 +0530

    Automorphic number.py

---
+++ Automorphic number.py
@@ -0,0 +1,16 @@
+n=677
+a=n*n
+if n<10:
+    if a%10==n:
+      print("automorphic number")
+    else:
+      print("not an automorphic number")
+else:
+     if a%100==n:
+      print("automorphic number")
+     else:
+      print("not an automorphic number")    
+
+
+    
+
