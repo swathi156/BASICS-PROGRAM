ac15b09f5238596b970f0cec8e3fcab37e2c3b3e
swathi156 <swathiprabhu016@gmail.com>
2025-04-12 11:33:41 +0530

    finding largest number in array and its factorial .py

---
+++ finding largest number in array and its factorial .py
@@ -0,0 +1,15 @@
+arr=[1,2,3,4,5,6]
+n=arr[0]
+for i in arr:
+    if i>n:
+        n=i
+print("largest number in array",n)
+
+f=1
+for i in range(1,n+1):
+    
+    f=f*i
+print("factorial",f)    
+
+
+            
